d591b594edd8e4aa05f67642448e7f3f1083c7d9
Oh-SulGi <oseulgi89@gmail.com>
2025-01-08 19:06:20 +0900

    feat: orderController 작성

--- src/main/java/kr/hhplus/be/server/api/controller/OrderController.java
+++ src/main/java/kr/hhplus/be/server/api/controller/OrderController.java
@@ -1,63 +1,49 @@
 package kr.hhplus.be.server.api.controller;
 
+import kr.hhplus.be.server.api.request.OrderRequest;
+import kr.hhplus.be.server.api.response.OrderResponse;
+import kr.hhplus.be.server.domain.order.OrderService;
+import kr.hhplus.be.server.exeption.GoodsOutOfStockException;
+import kr.hhplus.be.server.exeption.InvalidGoodsException;
+import kr.hhplus.be.server.exeption.InvalidUserException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 @RestController
 @RequestMapping("/api")
 public class OrderController {
+    private final OrderService orderService;
+    public OrderController(OrderService orderService) {
+        this.orderService = orderService;
+    }
 
     @PostMapping("/orders")
-    public ResponseEntity<Object> orderGoods(Long userId, Long goodsId, int quantity) {
-        List<Long> userList = new ArrayList<>(List.of(1L, 2L, 3L));
-        List<Map<String, Object>> goodsList = new ArrayList<>();
-
-        Map<String, Object> goodsInfo1 = new HashMap<>();
-        goodsInfo1.put("goodsId", 1L);
-        goodsInfo1.put("price", 10000L);
-        goodsInfo1.put("quantity", 30L);
-
-        Map<String, Object> goodsInfo2 = new HashMap<>();
-        goodsInfo2.put("goodsId", 2L);
-        goodsInfo2.put("price", 20000L);
-        goodsInfo2.put("quantity", 15L);
-
-        goodsList.add(goodsInfo1);
-        goodsList.add(goodsInfo2);
-
-        if (!userList.contains(userId)) {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("유저를 찾을 수 없습니다.");
+    public ResponseEntity<Object> orderGoods(Long userId, List<OrderRequest> orderRequestList) {
+        try {
+            List<OrderResponse> response = orderService.createOrder(userId, orderRequestList);
+            return ResponseEntity.ok(response);
+        } catch (InvalidUserException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+        } catch (InvalidGoodsException e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+        } catch (GoodsOutOfStockException e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
         }
+    }
 
-        for (Map<String, Object> goods : goodsList) {
-            if (goods.get("goodsId").equals(goodsId)) {
-                Long stockQuantity = (Long) goods.get("quantity");
-                if (quantity > stockQuantity) {
-                    return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("재고가 부족합니다.");
-                }
-                goods.put("quantity", stockQuantity - quantity);
-
-                Map<String, Object> order = new HashMap<>();
-                order.put("orderId", 1L);
-                order.put("orderDate", LocalDateTime.now());
-                order.put("userId", userId);
-                order.put("goodsId", goodsId);
-                order.put("orderedQuantity", quantity);
-
-                Long price = (Long) goods.get("price");
-                order.put("totalPrice", quantity * price);
-                return ResponseEntity.ok(order);
-            }
+    @GetMapping("/orders/{id}")
+    public ResponseEntity<Object> getMyOrder(Long userId) {
+        try {
+            List<OrderResponse> response = orderService.getMyOrder(userId);
+            return ResponseEntity.ok(response);
+        } catch (InvalidUserException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
         }
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body("상품을 찾을 수 없습니다.");
     }
 }
